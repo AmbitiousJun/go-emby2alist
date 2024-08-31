20e9094b626238018ff751f8cbcf65096d15d91c
ambitious <1535820771@qq.com>
2024-08-31 20:41:03 +0800

    feat: add console version info output

--- main.go
+++ main.go
@@ -6,7 +6,13 @@ import (
 	"log"
 )
 
+const CurrentVersion = "go-emby2alist => 1.0.0"
+const RepoArr = "https://github.com/AmbitiousJun/go-emby2alist"
+
 func main() {
+	log.Println("版本号: ", CurrentVersion)
+	log.Println("仓库地址: ", RepoArr)
+
 	log.Println("正在加载配置...")
 	if err := config.ReadFromFile("config.yml"); err != nil {
 		log.Fatal("加载失败", err)
