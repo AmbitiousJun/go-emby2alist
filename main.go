283d476a9228d0f9a72438b711aa1a9c0bd0dcf0
ambitious <1535820771@qq.com>
2024-09-05 21:28:14 +0800

    style: add banner

--- main.go
+++ main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"fmt"
 	"go-emby2alist/internal/config"
 	"go-emby2alist/internal/util/color"
 	"go-emby2alist/internal/web"
@@ -11,6 +12,7 @@ const CurrentVersion = "go-emby2alist => 1.0.3"
 const RepoArr = "https://github.com/AmbitiousJun/go-emby2alist"
 
 func main() {
+	printBanner()
 	log.Printf(color.ToPurple("版本号: %s"), CurrentVersion)
 	log.Printf(color.ToBlue("仓库地址: %s"), RepoArr)
 
@@ -23,4 +25,15 @@ func main() {
 	if err := web.Listen(8095); err != nil {
 		log.Fatal(err)
 	}
+}
+
+func printBanner() {
+	fmt.Println(`
+                                  _           ____       _ _     _   
+  __ _  ___         ___ _ __ ___ | |__  _   _|___ \ __ _| (_)___| |_ 
+ / _| |/ _ \ _____ / _ \ '_ | _ \| '_ \| | | | __) / _| | | / __| __|
+| (_| | (_) |_____|  __/ | | | | | |_) | |_| |/ __/ (_| | | \__ \ |_ 
+ \__, |\___/       \___|_| |_| |_|_.__/ \__, |_____\__,_|_|_|___/\__|
+ |___/                                  |___/                        
+	`)
 }
