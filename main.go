2fc5cd47fe64d07fa61408e207f9fe54c2a0e4aa
ambitious <1535820771@qq.com>
2024-09-11 20:46:38 +0800

    version: 1.0.5-beta-v1

--- main.go
+++ main.go
@@ -7,13 +7,11 @@ import (
 	"log"
 )
 
-const CurrentVersion = "go-emby2alist => 1.0.5-beta-v1"
-const RepoArr = "https://github.com/AmbitiousJun/go-emby2alist"
+const CurrentVersion = "1.0.5-beta-v1"
+const RepoAddr = "https://github.com/AmbitiousJun/go-emby2alist"
 
 func main() {
 	printBanner()
-	log.Printf(color.ToPurple("版本号: %s"), CurrentVersion)
-	log.Printf(color.ToBlue("仓库地址: %s"), RepoArr)
 
 	log.Println("正在加载配置...")
 	if err := config.ReadFromFile("config.yml"); err != nil {
@@ -27,12 +25,15 @@ func main() {
 }
 
 func printBanner() {
-	log.Println(`
+	log.Printf(color.ToBlue(`
                                   _           ____       _ _     _   
   __ _  ___         ___ _ __ ___ | |__  _   _|___ \ __ _| (_)___| |_ 
  / _| |/ _ \ _____ / _ \ '_ | _ \| '_ \| | | | __) / _| | | / __| __|
 | (_| | (_) |_____|  __/ | | | | | |_) | |_| |/ __/ (_| | | \__ \ |_ 
  \__, |\___/       \___|_| |_| |_|_.__/ \__, |_____\__,_|_|_|___/\__|
  |___/                                  |___/                        
-	`)
+ 
+ Repository: %s
+    Version: %s
+	`), CurrentVersion, RepoAddr)
 }
