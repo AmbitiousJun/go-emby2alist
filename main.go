73b10933ea523fdd459c2abf1302a44c0e7834eb
ambitious <1535820771@qq.com>
2024-10-14 22:32:20 +0800

    version: v1.1.0-beta-v1

--- main.go
+++ main.go
@@ -8,7 +8,7 @@ import (
 	"github.com/AmbitiousJun/go-emby2alist/internal/web"
 )
 
-const CurrentVersion = "v1.0.10"
+const CurrentVersion = "v1.1.0-beta-v1"
 const RepoAddr = "https://github.com/AmbitiousJun/go-emby2alist"
 
 func main() {
