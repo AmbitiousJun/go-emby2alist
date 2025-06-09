8e29200729c17943fe73bbef4eac8b875963fca8
ambitious <1535820771@qq.com>
2025-06-09 19:57:41 +0800

    refactor: remove go-proxyproto

--- internal/web/web.go
+++ internal/web/web.go
@@ -39,9 +39,7 @@ func Listen() error {
 
 // initRouter 初始化路由引擎
 func initRouter(r *gin.Engine) {
-	r.Use(gin.Recovery())
 	r.Use(referrerPolicySetter())
-	r.Use(proxyProtocolRealIPSetter())
 	r.Use(emby.ApiKeyChecker())
 	r.Use(emby.DownloadStrategyChecker())
 	if config.C.Cache.Enable {
@@ -56,30 +54,16 @@ func initRouter(r *gin.Engine) {
 // 出现错误时, 会写入 errChan 中
 func listenHTTP(errChan chan error) {
 	r := gin.New()
+	r.Use(gin.Recovery())
 	r.Use(CustomLogger(webport.HTTP))
 	r.Use(func(c *gin.Context) {
 		c.Set(webport.GinKey, webport.HTTP)
 	})
 	initRouter(r)
 	log.Printf(colors.ToBlue("在端口【%s】上启动 HTTP 服务"), webport.HTTP)
-
-	var err error
-	defer func() {
-		if err != nil {
-			errChan <- err
-			close(errChan)
-		}
-	}()
-
-	ln, err := initProxyProtocolLn(webport.HTTP)
-	if err != nil {
-		return
-	}
-
-	srv := &http.Server{
-		Handler: r,
-	}
-	err = srv.Serve(ln)
+	err := r.Run("0.0.0.0:" + webport.HTTP)
+	errChan <- err
+	close(errChan)
 }
 
 // listenHTTPS 在指定端口上监听 https 服务
@@ -87,6 +71,7 @@ func listenHTTP(errChan chan error) {
 // 出现错误时, 会写入 errChan 中
 func listenHTTPS(errChan chan error) {
 	r := gin.New()
+	r.Use(gin.Recovery())
 	r.Use(CustomLogger(webport.HTTPS))
 	r.Use(func(c *gin.Context) {
 		c.Set(webport.GinKey, webport.HTTPS)
@@ -95,23 +80,14 @@ func listenHTTPS(errChan chan error) {
 	log.Printf(colors.ToBlue("在端口【%s】上启动 HTTPS 服务"), webport.HTTPS)
 	ssl := config.C.Ssl
 
-	var err error
-	defer func() {
-		if err != nil {
-			errChan <- err
-			close(errChan)
-		}
-	}()
-
-	ln, err := initProxyProtocolLn(webport.HTTPS)
-	if err != nil {
-		return
-	}
-
 	srv := &http.Server{
+		Addr:    "0.0.0.0:" + webport.HTTPS,
 		Handler: r,
 	}
 	// 禁用 HTTP/2
 	srv.TLSNextProto = map[string]func(*http.Server, *tls.Conn, http.Handler){}
-	err = srv.ServeTLS(ln, ssl.CrtPath(), ssl.KeyPath())
+
+	err := srv.ListenAndServeTLS(ssl.CrtPath(), ssl.KeyPath())
+	errChan <- err
+	close(errChan)
 }
